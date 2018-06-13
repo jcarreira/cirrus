d7f2dbec72d2837df56ddc34d1dcc2ae48cc0193
Joao <joao@berkeley.edu>
2018-06-13 22:48:09 +0000

    Try again

--- bootstrap.sh
+++ bootstrap.sh
@@ -62,7 +62,7 @@ tar -xvzf $CURL_TAR
 mv curl-7.60.0 curl
 cd curl
 ./buildconf
-./configure --disable-shared --enable-static  --disable-sspi --disable-ftp --disable-file --disable-dict --disable-telnet --disable-tftp --disable-rtsp --disable-pop3 --disable-imap --disable-smtp --disable-gopher --disable-smb
+./configure --disable-shared --enable-static
 make -j 10
 cd ../
 
