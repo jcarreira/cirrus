20098a0f71674f9ff859ea4048c0fb1ce08638e1
Joao <joao@berkeley.edu>
2018-06-25 22:17:56 +0000

    Update

--- src/test_libnfs.cpp
+++ src/test_libnfs.cpp
@@ -6,6 +6,11 @@
 #include <libnfs.h>
 #include <cstring>
 
+#include <Utils.h>
+
+// to write larger sizes to EFS
+// the constant ZDR_ENCODEBUF_MINSIZE in lib/pdu.c needs to be changed
+
 int main() {
   struct nfs_context *nfs = nullptr;
   struct nfs_url *url = nullptr;
@@ -38,7 +43,7 @@ int main() {
   char* dummy_data = new char[10 * 1024 * 1024];
   memset(dummy_data, 0, 10 * 1024 * 1024);
 
-  for (int i = 100; i < 10 * 1024 * 1024; i += 100) {
+  for (int i = 100; i < 10 * 1024 * 1024; i += 10000) {
 
 
     for (int j = i - 100; j < i; ++j) {
@@ -46,12 +51,16 @@ int main() {
     }
 
     std::cout << "writing size: " << i << std::endl;
+    auto before = cirrus::get_time_us();
     ret = nfs_pwrite(nfs, file_handle, 0, i, dummy_data);
+    auto time_elapsed_us = cirrus::get_time_us() - before;
+    std::cout << "Time elapsed (us): " << time_elapsed_us << std::endl;
+    double mb_sec = 1.0 * (i/1024.0/1024) / time_elapsed_us * 1000.0*1000;
     if (ret <= 0) {
       continue;
       throw std::runtime_error("Error nfs_write " + std::to_string(ret));
     } else {
-      printf("size: %d worked\n", i);
+      printf("size: %d worked MB/s: %lf\n", i, mb_sec);
     }
 
     nfs_fsync(nfs, file_handle);
