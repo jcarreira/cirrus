61b5a0382fc1725c3a42fc1c3438baf6293f6f2a
yangryan0 <yangryan0@gmail.com>
2018-07-23 13:50:11 -0700

    Update setup.py

--- python/frontend/setup.py
+++ python/frontend/setup.py
@@ -4,15 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cirrus",
-    version="0.0.2",
-    author="jcarreira",
-    author_email="joao@berkeley.edu",
-    description="Serverless Machine Learning platform",
+    name='cirrus',
+    version='0.1dev',
+    description='Python library for Cirrus',
+    author="João Carreira",
+    author_email='joao [at] berkeley [dot] edu',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jcarreira/cirrus.git",
-    packages=setuptools.find_packages(),
+    license=open('LICENSE')
+    packages=find_packages(exclude=('tests', 'docs')),
     classifiers=(
         "Programming Language :: C++",
         "License :: OSI Approved :: MIT License",
