777ba9e3a2a546d221a7ec663fad099066e93282
Ryan Yang <yangryan0@gmail.com>
2018-07-26 22:47:00 +0000

    Added more time

--- tests/test_travis/mnist_test/test.sh
+++ tests/test_travis/mnist_test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for cmd in "timeout 120 ./tests/test_travis/mnist_test/test_sm" "timeout 100 ./tests/test_travis/mnist_test/worker_sm"; do
+for cmd in "timeout 140 ./tests/test_travis/mnist_test/test_sm" "timeout 120 ./tests/test_travis/mnist_test/worker_sm"; do
   eval ${cmd} &
-  sleep 1
+  sleep 10
 done
