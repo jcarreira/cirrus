e169b4b87dda75ce5aaf7f07bff02db50e48e340
Andrew M. Zhang <andrewmzhang@berkeley.edu>
2018-08-16 21:35:11 -0700

    Fixed S3Iterator

--- src/S3IteratorLibsvm.cpp
+++ src/S3IteratorLibsvm.cpp
@@ -18,75 +18,76 @@
 
 namespace cirrus {
 
-  S3IteratorLibsvm::S3IteratorLibsvm(const Configuration& c,
-      const std::string& s3_bucket,
-      const std::string& s3_key,
-      uint64_t file_size,
-      uint64_t minibatch_rows,
-      int worker_id,
-      bool random_access,
-      bool has_labels)
+S3IteratorLibsvm::S3IteratorLibsvm(const Configuration& c,
+                                   const std::string& s3_bucket,
+                                   const std::string& s3_key,
+                                   uint64_t file_size,
+                                   uint64_t minibatch_rows,
+                                   int worker_id,
+                                   bool random_access,
+                                   bool has_labels)
     : S3Iterator(c, has_labels),
-    s3_bucket(s3_bucket),
-    s3_key(s3_key),
-    file_size(file_size),
-    // s3_rows(s3_rows),
-    minibatch_rows(minibatch_rows),
-    minibatches_list(100000),
-    worker_id(worker_id),
-    re(worker_id),
-    random_access(random_access),
-    cur_index(0) {
-      std::cout << "S3IteratorLibsvm::Creating S3IteratorLibsvm" << std::endl;
+      s3_bucket(s3_bucket),
+      s3_key(s3_key),
+      file_size(file_size),
+      // s3_rows(s3_rows),
+      minibatch_rows(minibatch_rows),
+      minibatches_list(100000),
+      worker_id(worker_id),
+      re(worker_id),
+      random_access(random_access),
+      cur_index(0) {
+  std::cout << "S3IteratorLibsvm::Creating S3IteratorLibsvm" << std::endl;
 
-      // initialize s3
-      s3_client = std::make_shared<S3Client>();
+  // initialize s3
+  s3_client = std::make_shared<S3Client>();
 
-      for (uint64_t i = 0; i < read_ahead; ++i) {
-        pref_sem.signal();
-      }
+  for (uint64_t i = 0; i < read_ahead; ++i) {
+    pref_sem.signal();
+  }
 
-      sem_init(&semaphore, 0, 0);
+  sem_init(&semaphore, 0, 0);
 
-      thread =
-        new std::thread(std::bind(&S3IteratorLibsvm::threadFunction, this, c));
+  thread =
+      new std::thread(std::bind(&S3IteratorLibsvm::threadFunction, this, c));
 
-      // we fix the random seed but make it different for every worker
-      // to ensure each worker receives a different minibatch
-      if (random_access) {
-        srand(42 + worker_id);
-      }
-    }
+  // we fix the random seed but make it different for every worker
+  // to ensure each worker receives a different minibatch
+  if (random_access) {
+    srand(42 + worker_id);
+  }
+}
 
-  std::shared_ptr<SparseDataset> S3IteratorLibsvm::getNext() {
-    sem_wait(&semaphore);
-    ring_lock.lock();
+std::shared_ptr<SparseDataset> S3IteratorLibsvm::getNext() {
+  sem_wait(&semaphore);
+  ring_lock.lock();
 
-    // first discard empty queue
-    while (minibatches_list.front().size() == 0) {
-      auto queue_ptr = minibatches_list.pop();
-    }
-    auto ret = minibatches_list.front().back();
-    minibatches_list.front().pop_back();
-    num_minibatches_ready--;
-    ring_lock.unlock();
+  // first discard empty queue
+  while (minibatches_list.front().size() == 0) {
+    auto queue_ptr = minibatches_list.pop();
+  }
+  auto ret = minibatches_list.front().back();
+  minibatches_list.front().pop_back();
+  num_minibatches_ready--;
+  ring_lock.unlock();
 
-    // FIXME this should be calculating the local amount of memory
-    if (num_minibatches_ready < 200 && pref_sem.getvalue() < (int) read_ahead) {
+  // FIXME this should be calculating the local amount of memory
+  if (num_minibatches_ready < 200 && pref_sem.getvalue() < (int) read_ahead) {
 #ifdef DEBUG
-      std::cout << "getNext::pref_sem.signal" << std::endl;
+    std::cout << "getNext::pref_sem.signal" << std::endl;
 #endif
-      pref_sem.signal();
-    }
+    pref_sem.signal();
+  }
 
-    return ret;
+  return ret;
   }
 
   /**
    * Moves index forward while data[index] is a space
    * returns true if it ended on a digit, otherwise returns false
    */
-  bool S3IteratorLibsvm::ignoreSpaces(uint64_t& index, const std::string& data) {
+  bool S3IteratorLibsvm::ignoreSpaces(uint64_t& index,
+                                      const std::string& data) {
     while (isspace(data[index])) {
       index++;
     }
@@ -97,7 +98,7 @@ namespace cirrus {
    * while data[index] is a space character move forward until newline is found
    */
   bool S3IteratorLibsvm::ignoreSpacesNotNewline(uint64_t& index,
-      const std::string& data) {
+                                                const std::string& data) {
     while (data[index] != '\n' && isspace(data[index])) {
       index++;
     }
@@ -109,59 +110,61 @@ namespace cirrus {
    * Supports decimal values with dots (e.g., 3.14)
    */
   template <class T>
-    T S3IteratorLibsvm::readNum(uint64_t& index, std::string& data) {
-      if (!isdigit(data[index])) {
-        throw std::runtime_error("Error in the dataset");
-      }
+  T S3IteratorLibsvm::readNum(uint64_t& index, std::string& data) {
+    if (!isdigit(data[index])) {
+      throw std::runtime_error("Error in the dataset");
+    }
 
-      uint64_t index_fw = index;
-      // if float or double also accept dots
-      if constexpr(std::is_same<T, double>::value || 
-          std::is_same<T, float>::value) {
+    uint64_t index_fw = index;
+    // if float or double also accept dots
+    if
+      constexpr(std::is_same<T, double>::value ||
+                std::is_same<T, float>::value) {
         while (isdigit(data[index_fw]) || data[index_fw] == '.') {
           index_fw++;
         }
-      } else {
-        while (isdigit(data[index_fw])) {
-          index_fw++;
-        }
       }
-
-      char c = data[index_fw];
-      // need to temporarily add null termination to use sscanf
-      data[index_fw] = 0;
-
-      T result;
-      int sscanf_ret = 0;
-      if
-        constexpr(std::is_same<T, int>::value) {
-          sscanf_ret = sscanf(&data[index], "%d", &result);
-        }
-      else if
-        constexpr(std::is_same<T, double>::value) {
-          sscanf_ret = sscanf(&data[index], "%lf", &result);
-        }
-      else if
-        constexpr(std::is_same<T, float>::value) {
-          sscanf_ret = sscanf(&data[index], "%f", &result);
-        }
-      else if
-        constexpr(std::is_same<T, uint64_t>::value) {
-          sscanf_ret = sscanf(&data[index], "%lu", &result);
-        }
-      else {
-        throw std::runtime_error("Data type not supported");
+    else {
+      while (isdigit(data[index_fw])) {
+        index_fw++;
       }
-
-      if (sscanf_ret == EOF) {
-        throw std::runtime_error("Error reading number with sscanf");
-      }
-
-      // fix null-termination issue
-      data[index_fw] = c;  // repair
-      index = index_fw;
-      return result;
     }
+
+    char c = data[index_fw];
+    // need to temporarily add null termination to use sscanf
+    data[index_fw] = 0;
+
+    T result;
+    int sscanf_ret = 0;
+    if
+      constexpr(std::is_same<T, int>::value) {
+        sscanf_ret = sscanf(&data[index], "%d", &result);
+      }
+    else if
+      constexpr(std::is_same<T, double>::value) {
+        sscanf_ret = sscanf(&data[index], "%lf", &result);
+      }
+    else if
+      constexpr(std::is_same<T, float>::value) {
+        sscanf_ret = sscanf(&data[index], "%f", &result);
+      }
+    else if
+      constexpr(std::is_same<T, uint64_t>::value) {
+        sscanf_ret = sscanf(&data[index], "%lu", &result);
+      }
+    else {
+      throw std::runtime_error("Data type not supported");
+    }
+
+    if (sscanf_ret == EOF) {
+      throw std::runtime_error("Error reading number with sscanf");
+    }
+
+    // fix null-termination issue
+    data[index_fw] = c;  // repair
+    index = index_fw;
+    return result;
+  }
 
   bool S3IteratorLibsvm::buildDatasetCsv(
       const std::string& data,
@@ -204,7 +207,7 @@ namespace cirrus {
         if (!ignoreSpaces(index, data)) {
 #ifdef DEBUG
           std::cout << "ignoreSpaces did not find dig at index: " << index
-            << std::endl;
+                    << std::endl;
 #endif
           // did not end up in a digit
           if (data[index] == 0) {
@@ -242,7 +245,7 @@ namespace cirrus {
           }
           uint64_t ind = readNum<uint64_t>(index, data);
 #ifdef DEBUG
-          // std::cout << "index: " << index << " ind: " << ind << std::endl;
+  // std::cout << "index: " << index << " ind: " << ind << std::endl;
 #endif
           if (data[index] != ':') {
             return false;
@@ -251,12 +254,12 @@ namespace cirrus {
 
 #ifdef DEBUG
           std::string num_data = data.substr(index, 10);
-          // std::cout << "reading value: " << num_data << std::endl;
+  // std::cout << "reading value: " << num_data << std::endl;
 #endif
 
           FEATURE_TYPE value = readNum<FEATURE_TYPE>(index, data);
 #ifdef DEBUG
-          // std::cout << "index: " << index << " value: " << value << std::endl;
+  // std::cout << "index: " << index << " value: " << value << std::endl;
 #endif
 
           samples[sample].push_back(std::make_pair(ind, value));
@@ -276,7 +279,7 @@ namespace cirrus {
   }
 
   void S3IteratorLibsvm::readUntilNewline(uint64_t* index,
-      const std::string& data) {
+                                          const std::string& data) {
 #ifdef DEBUG
     std::cout << "reading until new line index: " << (*index) << std::endl;
 #endif
@@ -313,8 +316,8 @@ namespace cirrus {
       uint64_t start = get_time_us();
       if (!buildDatasetLibsvm(data, index, minibatch)) {
 #ifdef DEBUG
-        std::cout << "Finished text returning " << result.size() << " minibatches"
-          << std::endl;
+        std::cout << "Finished text returning " << result.size()
+                  << " minibatches" << std::endl;
 #endif
         return result;
       }
@@ -381,15 +384,15 @@ namespace cirrus {
     } else {
 #ifdef DEBUG
       std::cout << "cur_index: " << cur_index << " file_size: " << file_size
-        << std::endl;
+                << std::endl;
 #endif
       if (cur_index >= file_size) {
         // we reached the end
         cur_index = 0;
       }
       // we return <cur, cur + FETCH_SIZE>
-      auto ret =
-        std::make_pair(cur_index, std::min(cur_index + FETCH_SIZE, file_size));
+      auto ret = std::make_pair(cur_index,
+                                std::min(cur_index + FETCH_SIZE, file_size));
       cur_index += FETCH_SIZE;
       cur_index = std::min(cur_index, file_size);
       return ret;
@@ -400,8 +403,8 @@ namespace cirrus {
 #ifdef DEBUG
     double mb_s = size / elapsed_us * 1000.0 * 1000 / 1024 / 1024;
     std::cout << "received s3 obj"
-      << " elapsed: " << elapsed_us << " size: " << size
-      << " BW (MB/s): " << mb_s << "\n";
+              << " elapsed: " << elapsed_us << " size: " << size
+              << " BW (MB/s): " << mb_s << "\n";
 #endif
   }
 
@@ -417,7 +420,7 @@ namespace cirrus {
       std::pair<uint64_t, uint64_t> range = getFileRange(file_size);
 
       std::shared_ptr<std::ostringstream> s3_obj;
-try_start:
+    try_start:
       try {
         std::cout << "S3IteratorLibsvm: getting object" << std::endl;
         uint64_t start = get_time_us();
@@ -426,7 +429,7 @@ try_start:
 
 #ifdef DEBUG
         std::cout << "Read object with size: " << sstreamSize(*s3_obj)
-          << std::endl;
+                  << std::endl;
 #endif
 
         reportBandwidth(get_time_us() - start, sstreamSize(*s3_obj));
