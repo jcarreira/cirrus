7c1f4cc503b6b611db6526348f5ade7e38f34eb0
Joao Carreira <joao@berkeley.edu>
2018-08-28 04:24:18 +0000

    fix

--- src/LogisticSparseTaskS3.cpp
+++ src/LogisticSparseTaskS3.cpp
@@ -66,6 +66,7 @@ bool LogisticSparseTaskS3::get_dataset_minibatch(
 }
 
 void LogisticSparseTaskS3::run(const Configuration& config, int worker) {
+  s3_initialize_aws();
   std::cout << "Starting LogisticSparseTaskS3"
     << std::endl;
   uint64_t num_s3_batches = config.get_limit_samples() / config.get_s3_size();
