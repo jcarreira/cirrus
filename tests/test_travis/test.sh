3a76d4255aa2f3fc9803a3d526569389b7a7732f
Joao <joao@berkeley.edu>
2018-09-10 12:41:42 +0000

    changed sleep time

--- tests/test_travis/test.sh
+++ tests/test_travis/test.sh
@@ -2,5 +2,5 @@
 
 for cmd in "timeout 100 ./src/parameter_server --nworkers=10 --rank=1 --config=\"configs/criteo_kaggle.cfg\" --ps_port=1341" "timeout 75 ./src/parameter_server --nworkers=10 --rank=3 --config=\"configs/criteo_kaggle.cfg\" --ps_ip=\"127.0.0.1\" --ps_port=1341" "timeout 75 ./src/parameter_server --nworkers=10 --rank=3 --config=\"configs/criteo_kaggle.cfg\" --ps_ip=\"127.0.0.1\" --ps_port=1341"; do
   eval ${cmd} &
-  sleep 1
+  sleep 10
 done
