63a7c16878356df9374080468dd90880483ce49a
Joao <joao@berkeley.edu>
2018-09-10 11:33:20 +0000

    format fix

--- tests/test_travis/mnist_test/sm_worker.cpp
+++ tests/test_travis/mnist_test/sm_worker.cpp
@@ -34,9 +34,10 @@ int main() {
   while (1) {
     Dataset minibatch = train_dataset.random_sample(20);
     SoftmaxModel model = *(psi->get_sm_full_model(config));
-    auto gradient = model.minibatch_grad(minibatch.get_samples(),
-                                         dynamic_cast<float*>(minibatch.get_labels().get()),
-                                         20, config.get_learning_rate());
+    auto gradient =
+        model.minibatch_grad(minibatch.get_samples(),
+                             dynamic_cast<float*>(minibatch.get_labels().get()),
+                             20, config.get_learning_rate());
     gradient->setVersion(version++);
     SoftmaxGradient* smg = dynamic_cast<SoftmaxGradient*>(gradient.get());
     psi->send_sm_gradient(*smg);
