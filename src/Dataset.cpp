e16334171feaa68f6964e20a191a894ccceedd1a
Joao <joao@berkeley.edu>
2018-10-09 03:32:41 +0000

    comments fixed

--- src/Dataset.cpp
+++ src/Dataset.cpp
@@ -80,11 +80,12 @@ uint64_t Dataset::num_samples() const {
 void Dataset::check(const Configuration& config) const {
   const FEATURE_TYPE* l = labels_.get();
   for (uint64_t i = 0; i < num_samples(); ++i) {
-    if (!FLOAT_EQ(l[i], 1.0) && !FLOAT_EQ(l[i], 0.0) &&
-        config.get_model_type() != Configuration::SOFTMAX) {
-      throw std::runtime_error(
-          "Dataset::check_values wrong label value: " + std::to_string(l[i]));
-    } else if (config.get_model_type() == Configuration::SOFTMAX) {
+    if (config.get_model_type() != Configuration::SOFTMAX) {
+      if (!FLOAT_EQ(l[i], 1.0) && !FLOAT_EQ(l[i], 0.0)) {
+        throw std::runtime_error(
+            "Dataset::check_values wrong label value: " + std::to_string(l[i]));
+      }
+    } else {
       if (l[i] >= config.get_num_classes() or l[i] < 0 or floor(l[i]) != l[i]) {
         throw std::runtime_error("Dataset::check_values wrong label value: " +
                                  std::to_string(l[i]));
