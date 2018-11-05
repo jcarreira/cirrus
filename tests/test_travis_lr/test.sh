942b86e8fb5f576706c520ccda4dd264dfe2593b
Andrew M. Zhang <andrewmzhang@berkeley.edu>
2018-11-05 13:13:24 -0800

    Readded test_data csv's. Fixed errors in test.sh

--- tests/test_travis_lr/test.sh
+++ tests/test_travis_lr/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for cmd in "timeout 60 ./tests/test_travis/test_ps" "timeout 50 ./tests/test_travis/worker" "timeout 50 ./tests/test_travis/worker"; do
+for cmd in "timeout 60 ./tests/test_travis_lr/test_ps" "timeout 50 ./tests/test_travis_lr/worker" "timeout 50 ./tests/test_travis_lr/worker"; do
   eval ${cmd} &
   sleep 1
 done
--- tests/test_travis_lr_multiplePS/test.sh
+++ tests/test_travis_lr_multiplePS/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for cmd in "timeout 60 ./tests/test_travis/test_ps" "timeout 60 ./tests/test_travis/test_ps2" "timeout 50 ./tests/test_travis/worker" "timeout 50 ./tests/test_travis/worker"; do
+for cmd in "timeout 60 ./tests/test_travis_lr_multiplePS/test_ps" "timeout 60 ./tests/test_travis_lr_multiplePS/test_ps2" "timeout 50 ./tests/test_travis_lr_multiplePS/worker" "timeout 50 ./tests/test_travis_lr_multiplePS/worker"; do
   eval ${cmd} &
   sleep 1
 done
