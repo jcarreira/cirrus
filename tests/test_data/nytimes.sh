344c3bb028070beccae5bda8dc55a75e7cdc1832
Joao <joao@berkeley.edu>
2019-01-09 17:21:58 +0000

    add script to download nytimes dataset

---
+++ tests/test_data/nytimes.sh
@@ -0,0 +1,4 @@
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/docword.nytimes.txt.gz
+tar xvzf criteo.kaggle2014.svm.tar.gz
+
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.nytimes.txt
