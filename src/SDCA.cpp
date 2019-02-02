bb5205dd654446f4d2b0ab1ef23b2062c6ca81d8
Andy Wang <andyw1997@gmail.com>
2019-02-01 17:04:19 -0800

    removed debug messages

--- src/SDCA.cpp
+++ src/SDCA.cpp
@@ -14,26 +14,17 @@ void SDCA::sdca_update(std::unique_ptr<SparseLRSDCAModel>& lr_model,
     throw std::runtime_error("Error in dynamic cast");
   }
 
-  std::cout << "model w size = " << lr_model->weights_.size() << std::endl;
-  std::cout << "model a size = " << lr_model->coord_weights_.size() << std::endl;
-
-  std::cout << "w gradient: ";
   for (const auto& w : grad->w) {
     int index = w.first;
     FEATURE_TYPE value = w.second;
-    std::cout << "(" << w.first << "," << w.second << ")";
     lr_model->weights_[index] += value;
   }
-  std::cout << std::endl;
 
-  std::cout << "a gradient: ";
   for (const auto& a : grad->a) {
     int index = a.first;
     FEATURE_TYPE value = a.second;
-    std::cout << "(" << a.first << "," << a.second << ")";
     lr_model->coord_weights_[index] += value;
   }
-  std::cout << std::endl;
 }
 
 void SDCA::sgd_update(std::unique_ptr<cirrus::SparseLRModel>& lr_model,
