c3b01c354a6f9f262816f01f9e98a4822b260b23
Ryan <yangryan0@gmail.com>
2019-03-04 11:01:54 -0800

    pylint fixes

--- python/frontend/cirrus/examples/check_accuracy.py
+++ python/frontend/cirrus/examples/check_accuracy.py
@@ -1,21 +1,19 @@
 import time
 
-from cirrus import instance, automate, lr
-
 # Given a running ParameterServer object, this function checks
 # if the accuracy of the model increases above acc_threshold within
 # the timeout.
-def check_acc(server, acc_threshold = 0.73, timeout = 60):
-    l = ""
+def check_acc(server, acc_threshold=0.73, timeout=60):
+    total_line = ""
     for line in server.error_output().split("\n")[-10:]:
-        l = l + " " + line
+        total_line = total_line + " " + line
     timeout = time.time() + timeout
     recent_acc = -0.5
     while True:
         time.sleep(1)
         if time.time() > timeout:
             break
-        words = l.split(" ")
+        words = total_line.split(" ")
         accuracy = False
         for word in words:
             if accuracy:
@@ -24,9 +22,10 @@ def check_acc(server, acc_threshold = 0.73, timeout = 60):
             else:
                 if word == "Accuracy:":
                     accuracy = True
-    if recent_acc >= 0.72:
-        print("Test passed")
+    if recent_acc >= acc_threshold:
+        print "Test passed"
     elif recent_acc < 0:
-        print("Test failed. There was an error running ErrorSparseTask or wait for about 30 seconds for ErrorSparseTask.")
+        print "Test failed. There was an error running ErrorSparseTask \
+                or wait for about 30 seconds for ErrorSparseTask."
     else:
-        print("Test failed. Accuracy not high enough." + "(" + str(recent_acc) + ")")
+        print "Test failed. Accuracy not high enough." + "(" + str(recent_acc) + ")"
