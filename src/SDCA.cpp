02fd8b46c203f8ab300ba97e4bbd1aec9058f68e
Andy Wang <andyw1997@gmail.com>
2019-03-06 15:46:07 -0800

    changed debug to w

--- src/SDCA.cpp
+++ src/SDCA.cpp
@@ -14,20 +14,20 @@ void SDCA::sdca_update(std::unique_ptr<SparseLRSDCAModel>& lr_model,
     throw std::runtime_error("Error in dynamic cast");
   }
 
+  std::cout << "w vals " << std::endl;
   for (const auto& w : grad->w) {
     int index = w.first;
     FEATURE_TYPE value = w.second;
     lr_model->weights_[index] += value;
+    std::cout << index << "," << value << " ";
   }
-  std::cout << "a vals " << std::endl;
+  std::cout << std::endl;
 
   for (const auto& a : grad->a) {
     int index = a.first;
     FEATURE_TYPE value = a.second;
     lr_model->coord_weights_[index] += value;
-    std::cout << index << "," << value << " ";
   }
-  std::cout << std::endl;
 }
 
 void SDCA::sgd_update(std::unique_ptr<cirrus::SparseLRModel>& lr_model,
