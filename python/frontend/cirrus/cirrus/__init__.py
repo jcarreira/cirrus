065602755fc63e71750cb81ab084f327ae119fb2
Ryan <yangryan0@gmail.com>
2019-03-07 13:14:16 -0800

    cv line removed

--- python/frontend/cirrus/cirrus/__init__.py
+++ python/frontend/cirrus/cirrus/__init__.py
@@ -1,6 +1,5 @@
 from .CostModel import CostModel
 from .GridSearch import GridSearch
-from .CrossValidation import CrossValidation
 from .cf import CollaborativeFiltering
 from .lr import LogisticRegression
 from .utils import *
