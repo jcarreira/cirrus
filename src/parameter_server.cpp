9b09c6f7ca0e39f35a8083e3419de48e5b0f0db7
Andy Wang <andyw1997@gmail.com>
2019-03-06 19:00:45 -0800

    fixed formatting, again

--- src/parameter_server.cpp
+++ src/parameter_server.cpp
@@ -55,18 +55,16 @@ void run_tasks(int rank,
             features_per_sample, nworkers, rank, ps_ip, ps_port);
         lt.run(config, rank - WORKERS_BASE);
       } else {
-        cirrus::LogisticSparseTaskS3 lt(
-            features_per_sample,
-            batch_size, samples_per_batch, features_per_sample,
-            nworkers, rank, ps_ip, ps_port);
+        cirrus::LogisticSparseTaskS3 lt(features_per_sample, batch_size,
+                                        samples_per_batch, features_per_sample,
+                                        nworkers, rank, ps_ip, ps_port);
         lt.run(config, rank - WORKERS_BASE, test_iters);
       }
     } else if (config.get_model_type()
                == cirrus::Configuration::COLLABORATIVE_FILTERING) {
-      cirrus::MFNetflixTask lt(
-          0,
-          batch_size, samples_per_batch, features_per_sample,
-          nworkers, rank, ps_ip, ps_port);
+      cirrus::MFNetflixTask lt(0, batch_size, samples_per_batch,
+                               features_per_sample, nworkers, rank, ps_ip,
+                               ps_port);
       lt.run(config, rank - WORKERS_BASE, test_iters);
     } else {
       exit(-1);
